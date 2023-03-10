5f309a0ada8671f297e84821663d750d3d255d40
fabian lopez <fabianlopez928@gmail.com>
2023-03-10 10:04:30 -0500

    renderizandoPokemonCard

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { StyleSheet,View, Text, FlatList } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import PokemonCard from "./PokemonCard.js";
 
 export default function PokemonList(props) {
   const { pokemons } = props;
+  
   return (
     <FlatList
       data={pokemons}
       numColumns={2}
       showsVerticalScrollIndicator={false}
       keyExtractor={(pokemon) => String(pokemon.id)}
-      renderItem={({ item }) => <Text>{item.name}</Text>}
+      renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContainer}
     />
   );
 }
 
-
-
-const styles=StyleSheet.create({
-  flatListContainer:{
-    paddingHorizontal:5
-  }
-})
+const styles = StyleSheet.create({
+  flatListContainer: {
+    paddingHorizontal: 5,
+  },
+});
